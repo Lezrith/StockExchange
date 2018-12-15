f8eedc7983dcc4ec68a851dd94960dfd2d8d5aab
Jan Bajer <jan.bajer@gmail.com>
2018-12-15 12:57:44 +0100

    Add db configuration

--- Data/CassandraContext.cs
+++ Data/CassandraContext.cs
@@ -25,11 +25,41 @@ namespace Data
                 .Build();
             this.session = cluster.Connect(options.Keyspace);
             this.mapper = new Mapper(this.session);
+            this.ConfigureCluster();
         }
 
         public string GetCqlVersion()
         {
             return this.mapper.Single<string>("SELECT cql_version FROM system.local");
+        }
+
+        private void ConfigureCluster()
+        {
+            var keyspaceCreate = this.session.Execute("CREATE KEYSPACE IF NOT EXISTS stock_exchange " +
+                "WITH REPLICATION = { 'class': 'SimpleStrategy', 'replication_factor': 3 };");
+            var keyspaceUse = this.session.Execute("USE stock_exchange");
+            var orderCreate = this.session.Execute("CREATE TABLE IF NOT EXISTS stock_exchange.orders (" +
+                "OrderId uuid," +
+                "StockSymbol text," +
+                "SubmitterId uuid," +
+                "SubmitterName text," +
+                "Quantity int," +
+                "OrderType int," +
+                "PricePerUnit decimal," +
+                "Date timestamp," +
+                "LockedBy set<uuid>," +
+                "PRIMARY KEY (StockSymbol, OrderType));");
+            var transactionCreate = this.session.Execute("CREATE TABLE IF NOT EXISTS stock_exchange.transactions (" +
+                "TransactionId uuid," +
+                "StockSymbol text," +
+                "BuyerId uuid," +
+                "BuyerName text," +
+                "SelledId uuid," +
+                "SellerName text," +
+                "Quantity int," +
+                "PricePerUnit decimal," +
+                "Date timestamp," +
+                "PRIMARY KEY ((StockSymbol), Date));");
         }
     }
 }
