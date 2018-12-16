3166b5ccfe2763cd4591bc2817f7a9118eed2c04
Krzysztof Tomczak <krzysztof.g.tomczak@student.put.poznan.pl>
2018-12-16 14:32:57 +0100

    Added matcher

---
+++ MatcherLogic/Matcher.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using Data;
+using Model;
+
+namespace MatcherLogic
+{
+    public class Matcher
+    {
+        private readonly IList<string> companies;
+        private readonly CassandraContext context;
+        private readonly Random rng;
+        private readonly TimeSpan period;
+        private readonly Guid matcherId;
+
+        public Matcher(IList<string> companies, CassandraContext context, Random rng, TimeSpan period)
+        {
+            this.companies = companies ?? throw new ArgumentNullException(nameof(companies));
+            this.context = context ?? throw new ArgumentNullException(nameof(context));
+            this.rng = rng ?? throw new ArgumentNullException(nameof(rng));
+            this.period = period;
+            this.matcherId = new Guid();
+        }
+
+        public void Run()
+        {
+            while (true)
+            {
+                var company = this.companies[this.rng.Next(this.companies.Count)];
+                this.CreateTransaction(company);
+                Thread.Sleep(this.period);
+            }
+        }
+
+        private void CreateTransaction(string company)
+        {
+            var orders = this.context.FetchOrders(company).ToList();
+            var purchases = orders.Where(o => o.OrderType == OrderType.Purchase);
+            var sales = orders.Where(o => o.OrderType == OrderType.Sale);
+            var (sale, purchase) = sales
+                .Join(purchases, s => s, p => p, (s, p) => (s, p), new OrderMatchComparer())
+                .FirstOrDefault();
+            var toLock = new List<Order> { sale, purchase };
+            this.context.LockOrders(toLock, this.matcherId);
+            if (this.context.HaveLock(toLock, this.matcherId))
+            {
+                this.context.MakeTransaction(purchase, sale);
+            }
+            else
+            {
+                this.context.UnlockOrders(toLock, this.matcherId);
+            }
+        }
+
+        private class OrderMatchComparer : IEqualityComparer<Order>
+        {
+            public bool Equals(Order x, Order y)
+            {
+                if (x.OrderType == y.OrderType)
+                {
+                    return false;
+                }
+                var purchase = x.OrderType == OrderType.Purchase ? x : y;
+                var sale = x.OrderType == OrderType.Sale ? x : y;
+
+                return sale.Quantity >= purchase.Quantity && sale.PricePerUnit <= purchase.PricePerUnit;
+            }
+
+            public int GetHashCode(Order obj)
+            {
+                return obj.OrderId.GetHashCode();
+            }
+        }
+    }
+}
